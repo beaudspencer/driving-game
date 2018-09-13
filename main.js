467d96d94b7bb3b023bfdac7274ef5f333051672
Beau Spencer <bdspencer416@gmail.com>
2018-09-13 11:10:17 -0700

    ajusted the styling of the car to make the size practical. #1

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ class Car {
 
 var $carImg = document.createElement('img')
 $carImg.setAttribute('src', 'car-black.png')
+$carImg.setAttribute('style', 'width: 4rem;')
 document.body.appendChild($carImg)
 
 var car = new Car($carImg, 10, 'north', [0, 0])
